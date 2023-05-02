5ed51f83c8d92d71b88372e4e968fbbcb22fdfda
Rune K. Svendsen <runesvend@gmail.com>
2023-05-02 10:19:01 -0400

    Parser succeeds \o/

--- src/Action/Tmp.hs
+++ src/Action/Tmp.hs
@@ -10,7 +10,7 @@ import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified System.IO as IO
-import Control.Monad (forM_, unless)
+import Control.Monad (forM_, unless, guard, void)
 import qualified Text.HTML.TagSoup as Html
 import Text.HTML.TagSoup (Tag(..))
 import Text.Show.Pretty
@@ -21,83 +21,30 @@ import Data.Either (fromRight, lefts, rights)
 
 import qualified Action.Tmp.ParseTag as P
 import qualified Text.Megaparsec as MP
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, isJust)
 import Control.Monad.Fix (mfix)
 import Data.Function (fix)
 import qualified Data.Set as Set
 import qualified Text.Megaparsec.Debug as MP
 import Data.Void (Void)
+import Debug.Trace (trace)
+import Control.Applicative ((<|>))
 
-main fn = do
-  content <- C8.readFile fn
-  let cLines = C8.lines content
-  forM_ cLines $ \cLine -> do
-    let txtLine = T.pack . C8.unpack $ cLine
-    either
-      (\err -> IO.hPutStrLn IO.stderr $ "Failed to decode line '" <> T.unpack txtLine <> "' error: " <> err)
-      T.putStrLn
-      (HTML.decode txtLine)
-    -- TODO: print warning on Left
+main fn = testParseHtml
 
-readHtml modName fn = do
-  content <- BS.readFile fn
-  let tags = Html.parseTags content
-  let parsedTagsE = parseTags tags
-      parsedTags = fromRight (error ":(") parsedTagsE
-  forM_ parsedTags (BS.putStrLn . mconcat . intersperse " " . renderFun)
-  where
-    parseTags :: [Tag BS.ByteString] -> Either String [Fun BS.ByteString]
-    parseTags tags =
-      let go :: ([Tag BS.ByteString], [Fun BS.ByteString])
-              -> Either String ([Tag BS.ByteString], [Fun BS.ByteString])
-          go ([], accum) = Right ([], accum)
-          go (state@(_ : stateRest), accum) =
-            case parseFun modName state of
-              Left err -> Left err
-              Right Nothing -> go (stateRest, accum)
-              Right (Just (fun, stateRest')) -> go (stateRest', fun : accum)
-      in snd <$> go (tags, [])
+testParseHtml = do
+  eRes <- parseFile
+    "GHC.IO.Encoding.UTF16"
+    "/nix/store/hs8dbwgs3mxj6qnl3zxbb8rp22722fv6-ghc-8.6.5-doc/share/doc/ghc/html/libraries/base-4.12.0.0/GHC-IO-Encoding-UTF16.html"
+  either
+    print -- MP.errorBundlePretty
+    (\res -> do
+      putStrLn "Success! Results:"
+      print res
+      -- (BS.putStrLn . mconcat . intersperse " " . renderFun) res
+    )
+    eRes
 
-parseFun
-  :: (Eq str, Data.String.IsString str, Semigroup str, Show str)
-  => str -- ^ Module name
-  -> [Tag str] -- ^ Parser input
-  -> Either String (Maybe (Fun str, [Tag str]))
-  -- ^ Right: on successful parse a Fun and the remainder of the input
-parseFun modName = \case
-  [] -> Right Nothing
-  TagOpen "p" [ ( "class" , "src" ) ]
-    : TagOpen "a" tagsName -- [ ( "id" , "v:fromLazyText" ) , ( "class" , "def" ) ]
-    : TagText name
-    : TagClose "a"
-    : TagText " :: "
-    : TagOpen
-        "a"
-        tagsSrc
-        -- [ ( "href"
-        --   , "file:///nix/store/hs8dbwgs3mxj6qnl3zxbb8rp22722fv6-ghc-8.6.5-doc/share/doc/ghc/html/libraries/text-1.2.3.1/Data-Text-Lazy.html#t:Text"
-        --   )
-        -- , ( "title" , "Data.Text.Lazy" )
-        -- ]
-    : TagText _ -- "Text"
-    : TagClose "a"
-    : TagText " -> "
-    : TagOpen
-        "a"
-        tagsDst
-        -- [ ( "href" , "Blaze-ByteString-Builder.html#t:Builder" )
-        -- , ( "title" , "Blaze.ByteString.Builder" )
-        -- ]
-    : TagText _ -- "Builder"
-    : TagClose "a"
-    : xs ->
-        Fun (modName <> "." <> name)
-          <$> ((:[]) <$> lookupE "title" tagsSrc)
-          <*> ((:[]) <$> lookupE "title" tagsDst)
-            <&> \fun -> Just (fun, xs)
-  _ -> Right Nothing
-  where
-    lookupE name tags = maybe (Left $ show name <> " not found in " <> show tags) Right (lookup name tags)
 
 data Fun str = Fun
   { funName :: str
@@ -118,113 +65,115 @@ match =
     match' :: (Maybe a -> Maybe Bool) -> a -> Bool
     match' f = \i -> fromMaybe False (f $ Just i)
 
-p
-  :: (P.MonadParsec e s m, e ~ Void, P.StringLike str, P.Token s ~ Tag str, Show str, Semigroup str)
-  => str
-  -> m (Fun str)
-p modName = do
-  P.tagOpen "p"
-  P.tagOpen "a"
-  name <- P.tagText >>= \(TagText name) -> pure name
-  P.tagClose "a"
-  match $ \mi -> do
+-- p :: (MP.VisualStream s, MP.Token s ~ Tag str, Show str, P.StringLike str, Semigroup str) =>
+--   str -> MP.ParsecT Void s m (Fun str)
+pName modName = do
+  -- MP.dbg "p_open" $ P.tagOpen "p"
+  MP.dbg "p_open" $ match $ \mi -> do
+    TagOpen "p" attrs <- mi
+    pure $ lookup "class" attrs == Just "src"
+  -- MP.dbg "a_open" $ P.tagOpen "a"
+  MP.dbg "a_open" $ match $ \mi -> do
+    TagOpen "a" attrs <- mi
+    pure $ lookup "class" attrs == Just "def"
+  name <- MP.dbg "name" $ P.tagText >>= \(TagText name) -> pure $ unlines [show name] `trace` name
+  MP.dbg "a_close" $ P.tagClose "a"
+  MP.dbg "has type" $ match $ \mi -> do
     TagText t <- mi
     pure $ t == " :: "
-  tagsSrc <- flip fix [] $ \loop accum ->
-    P.anyTag >>= \case
-      TagText " -> " ->
-        pure accum
+  -- 1. TagOpen containing module name in the "title" attribute
+  firstTypeModuleTag@(TagOpen "a" attrs) <- match $ \mi -> do -- TODO: write a combinator that can return what it matches on
+    TagOpen "a" attrs <- mi
+    pure $ isJust $ lookup "title" attrs
+  let firstTypeModule = fromMaybe (error $ "BUG: firstTypeTag: " <> show firstTypeModuleTag) $ lookup "title" attrs
+  -- 2. TagText containing the (unqualified) type name
+  TagText firstTypeName <- match $ \mi -> do -- TODO: write a combinator that can return what it matches on
+    TagText _ <- mi
+    pure True
+  -- 3. TagClose "a"
+  P.tagClose "a"
+  toEndOfSignature <- pEndFunctionSignature -- >>= \eos -> pure $ ("toEndOfSignature: " <> show eos) `trace` eos
+  pure (name, firstTypeModule <> "." <> firstTypeName, toEndOfSignature)
+
+
+pSrc name = do
+  tagsSrc <- flip fix [] $ \loop accum -> do
+    someTag <- P.anyTag >>= \tag' -> pure $ ("tagsSrc loop: " <> show tag') `trace` tag'
+    case someTag of
+      TagText " -> " -> do
+        let res = reverse accum
+        pure $ ("got src: " <> show res) `trace` reverse accum
       other ->
         loop (other : accum)
-  src <- extractSomething tagsSrc
+  src <- extractSomething tagsSrc >>= \src' -> pure $ ("extracted src: " <> show src') `trace` src'
+
+  pure (name, src)
+
+-- pEndFunctionSignature
+--   :: (MP.MonadParsec e s m, P.StringLike str, MonadFail m, MP.Token s ~ Tag str, Show str, MP.VisualStream s) => m [Tag str]
+pEndFunctionSignature = MP.dbg "pEndFunctionSignature" $
+  MP.manyTill P.anyTag $ match $ \mi -> do
+      TagOpen "a" attrs <- mi
+      pure $ lookup "class" attrs == Just "link"
+
+pRest modName name src = do
   tagsDst <- flip fix [] $ \loop accum ->
-    P.anyTag >>= \case
+    MP.dbg ("tagsDst") $ P.anyTag >>= \case
       TagOpen "a" attrs | lookup "class" attrs == Just "link" ->
-        pure accum
+        pure $ reverse accum
       other ->
-        loop (other : accum)
+        ("tagsDst loop: " <> show other) `trace` loop (other : accum)
   dst <- extractSomething tagsDst
+  pure $ ("got dst: " <> show dst) `trace` ()
   pure $ Fun (modName <> "." <> name) src dst
 
+lookupE name tags = maybe (Left $ show name <> " not found in " <> show tags) Right (lookup name tags)
+
+mkFancyError errLst =
+  MP.FancyError 0 $ Set.fromList (map MP.ErrorFail errLst :: [MP.ErrorFancy Void])
+
+extractSomething tags = do
+  let res = tags <&> \case
+        TagText t -> Right t
+        TagOpen _ attrs -> lookupE "title" attrs
+        other -> Left $ "Unexpected tag: " <> show other
+      src = rights res
+      errors = lefts res
+  unless (null errors) $
+    MP.parseError $ mkFancyError errors
+  pure src
+
+anyTagOrEof
+  :: (P.StringLike str, MP.MonadParsec e s f, MP.Token s ~ Tag str)
+  => f (Either () (Tag str))
+anyTagOrEof = (Right <$> P.anyTag) <|> (Left <$> MP.eof)
+
+type Res = (BS.ByteString, BS.ByteString, [Tag BS.ByteString])
+data Result str a = UselessTag (Tag str) | Good Res | EOF
+
+newParser modName =
+  go []
   where
-    lookupE name tags = maybe (Left $ show name <> " not found in " <> show tags) Right (lookup name tags)
+    go accum = do
+      r <- (Good <$> pName') <|> (UselessTag <$> P.anyTag) <|> (EOF <$ MP.eof)
+      case r of
+        Good res -> do
+          go $ res : accum
+        UselessTag _ ->
+          go accum
+        EOF ->
+          pure accum
 
-    mkFancyError errLst =
-      MP.FancyError 0 $ Set.fromList (map MP.ErrorFail errLst :: [MP.ErrorFancy Void])
+    parser = (Good <$> pName') <|> (UselessTag <$> P.anyTag) <|> (EOF <$ MP.eof)
 
-    extractSomething tags = do
-      let res = tags <&> \case
-            TagText t -> Right t
-            TagOpen _ attrs -> lookupE "title" attrs
-            other -> Left $ "Unexpected tag: " <> show other
-          src = rights res
-          errors = lefts res
-      unless (null errors) $
-        MP.parseError $ mkFancyError errors
-      pure src
-
--- <a href="src/GHC-IO-Encoding-UTF16.html#utf16_decode" class="link">Source</a>
-
-parse modName =
-  MP.parse (MP.dbg "" p') "lol"
-  where
-    p' = MP.skipManyTill P.anyTag debugP -- (p modName)
-
-    debugP = do
-      -- P.tagOpen "!DOCTYPE"
-      -- P.tagOpen "html"
-      MP.dbg "1" $ P.tagOpen "head"
-      MP.dbg "2" $ P.tagOpen "meta"
-      MP.dbg "3" $ P.tagClose "meta"
+    pName' = pName modName --  >>= \res -> pure $ ("pName: " <> show res) `trace` res
 
 parseFile ::
-  p
+  BS.ByteString
   -> FilePath
-  -> IO
-      (Either
-          (MP.ParseErrorBundle [Tag BS.ByteString] Void) (Tag BS.ByteString))
+  -> IO (Either (MP.ParseErrorBundle [Tag BS.ByteString] Void) [Res])
 parseFile modName fn = do
   content <- BS.readFile fn
   let tags = Html.parseTags content
-  pPrint tags
-  pure $ parse modName tags
-
-testParseHtml = do
-  eRes <- parseFile
-    "GHC.IO.Encoding.UTF16"
-    "/nix/store/hs8dbwgs3mxj6qnl3zxbb8rp22722fv6-ghc-8.6.5-doc/share/doc/ghc/html/libraries/base-4.12.0.0/GHC-IO-Encoding-UTF16.html"
-  either
-    print -- MP.errorBundlePretty
-    (\res -> do
-      putStrLn "Success! Results:"
-      print res
-      -- (BS.putStrLn . mconcat . intersperse " " . renderFun) res
-    )
-    eRes
-
-input =
-    [ TagOpen "p" [ ( "class" , "src" ) ]
-    , TagOpen "a" [ ( "id" , "v:fromLazyText" ) , ( "class" , "def" ) ]
-    , TagText "someName"
-    , TagClose "a"
-      -- , TagClose "b"
-    , TagText " :: "
-    , TagOpen
-        "a"
-        [ ( "href"
-          , "file:///nix/store/hs8dbwgs3mxj6qnl3zxbb8rp22722fv6-ghc-8.6.5-doc/share/doc/ghc/html/libraries/text-1.2.3.1/Data-Text-Lazy.html#t:Text"
-          )
-        , ( "title" , "Data.Text.Lazy" )
-        ]
-    , TagText "Text"
-    , TagClose "a"
-    , TagText " -> "
-    , TagOpen
-        "a"
-        [ ( "href" , "Blaze-ByteString-Builder.html#t:Builder" )
-        , ( "title" , "Blaze.ByteString.Builder" )
-        ]
-    , TagText "Builder"
-    , TagClose "a"
-    , TagText "blah"
-    ]
+  -- pPrint tags
+  pure $ MP.parse (newParser modName) "lol" tags
